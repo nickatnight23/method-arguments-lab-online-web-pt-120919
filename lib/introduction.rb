f9dd250469ca2c4bfee45c23e5e55140f1d7de43
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-09 18:56:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introudction_with_language (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introudction_with_language (Dan, Ember.js)
+  puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}."
 end
