f1782f2270f80b3172c65c9fd0a716277ac739e1
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-09 18:56:30 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introudction_with_language (Dan, Ember.js)
+  puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}."
+end
